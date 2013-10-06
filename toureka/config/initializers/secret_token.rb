1ff781761297c27886d85218eb03963776bfd8ab
Ved Ratn Dixit <ved050493@gmail.com>
2013-10-06 19:48:58 +0530

    Basic rails project structure made

---
+++ toureka/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toureka::Application.config.secret_token = '3dd66a2115b467daeb019a898f563233f363f57a76473655ae58eca1fc43266270538bdb8eca756e2767d571d59e6bee8a2de67d7b2d2923364edeb5aa6eefec'
