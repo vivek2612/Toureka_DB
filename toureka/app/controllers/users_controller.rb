4b50b0cc4c271240b22c69378c8f601bd3f989fb
Ved Ratn Dixit <ved050493@gmail.com>
2013-10-30 20:27:43 +0530

    Controller for backend written but not debugged

--- toureka/app/controllers/users_controller.rb
+++ toureka/app/controllers/users_controller.rb
@@ -84,7 +84,6 @@ class UsersController < ApplicationController
   end
 
   def writer_final
-    @user = User.find(params[:id])
     stateIdNow = State.where(:name=>session[:stateName])[0].id
     if params[:DN1] # ADD NEW DISTRICT
       if District.exists?(:name=>params[:DN1], :state_id=>stateIdNow)
@@ -128,7 +127,248 @@ class UsersController < ApplicationController
   end
 
   def writer_done
-    puts params.keys
+    keys = params.keys
+    tskeys = keys.select{|x| x[0]=='T'}
+    tsdkeys = tskeys.select{|x| x[2]=='D'}
+    tskeys = tskeys - tsdkeys
+    epkeys = keys.select{|x| x[0]=='E'}
+    epdkeys = keys.select{|x| x[2]=='D'}
+    epkeys = epkeys - epdkeys
+    hkeys = keys.select{|x| x[0]=='H'}
+    hdkeys = keys.select{|x| x[1]=='D'}
+    hkeys = hkeys - hdkeys
+    ltskeys = keys.select{|x| x[0]=='L'}
+    ltsDkeys = keys.select {|x| x[3]=='D' }
+    ltskeys = ltskeys - ltsdkeys
+    i = 0
+    while i < tskeys.size
+      name = params[tskeys[i]]
+      lat = params[tskeys[i+1]]
+      long = params[tskeys[i+2]]
+      desc = params[tskeys[i+3]]
+      cat = params[tskeys[i+4]]
+      if(TouristSpot.exists?(:name=>name, :category=>cat))
+        t = TouristSpot.where(:name=>name, :category=>cat)[0]
+        unless t.update_attributes(:latitude=>lat,:longitude=>long,:description=>desc)
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of TouristSpot #{@TS.name}"
+          render 'writer_final.html.erb'
+        end
+      else
+        t = TouristSpot.new
+        t.name=name
+        t.category=cat
+        t.latitude=lat
+        t.longitude=long
+        t.description=desc
+        if t.save?
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of TouristSpot #{@TS.name}"
+          render 'writer_final.html.erb'
+        else
+          t.save
+        end
+      end
+      i+=5
+    end
+    i = 0
+    while i < tsdkeys.size
+      name=params[tsdkeys[i]]
+      cat=params[tsdkeys[i+1]]
+      if(TouristSpot.exists?(:name=>name, :category=>cat))
+        t = TouristSpot.where(:name=>name, :category=>cat)[0]
+        t.destroy
+      else
+        flash[:error]="TouristSpot does not exist"
+        @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @user = User.find(params[:id])
+        render 'writer_final.html.erb'
+      end
+      i+=2
+    end
+
+    # Now lets modify hotels
+    i = 0
+    while i < hkeys.size
+      name = params[hkeys[i]]
+      lat = params[hkeys[i+1]]
+      long = params[hkeys[i+2]]
+      desc = params[hkeys[i+3]]
+      if(Hotel.exists?(:name=>name, :stateName=>session[:stateName], :districtName=>session[:districtName]))
+        t = Hotel.where(:name=>name, :stateName=>session[:stateName], :districtName=>session[:districtName])[0]
+        unless t.update_attributes(:latitude=>lat,:longitude=>long,:description=>desc)
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of TouristSpot #{@TS.name}"
+          render 'writer_final.html.erb'
+        end
+      else
+        t = Hotel.new
+        t.name=name
+        t.latitude=lat
+        t.longitude=long
+        t.description=desc
+        if t.save?
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of TouristSpot #{@TS.name}"
+          render 'writer_final.html.erb'
+        else
+          t.save
+        end
+      end
+      i+=4
+    end
+    i = 0
+    while i < hdkeys.size
+      name=params[hdkeys[i]]
+      if(Hotel.exists?(:name=>name, :stateName=>session[:stateName], :districtName=>session[:districtName]))
+        t = Hotel.where(:name=>name, :stateName=>session[:stateName], :districtName=>session[:districtName])[0]
+        t.destroy
+      else
+        flash[:error]="Hotel does not exist"
+        @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @user = User.find(params[:id])
+        render 'writer_final.html.erb'
+      end
+      i+=1
+    end
+
+    # Now lets modify EntryPoint
+    i = 0
+    while i < epkeys.size
+      name = params[epkeys[i]]
+      lat = params[epkeys[i+1]]
+      long = params[epkeys[i+2]]
+      type = params[epkeys[i+3]]
+      if(EntryPoint.exists?(:name=>name, :entryType=>type, :stateName=>session[:stateName], :districtName=>session[:districtName]))
+        t = EntryPoint.where(:name=>name, :entryType=>type, :stateName=>session[:stateName], :districtName=>session[:districtName])
+        unless t.update_attributes(:latitude=>lat,:longitude=>long)
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of TouristSpot #{@TS.name}"
+          render 'writer_final.html.erb'
+        end
+      else
+        t = EntryPoint.new
+        t.name=name
+        t.latitude=lat
+        t.longitude=long
+        t.entryType=type
+        if t.save?
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of Hotel #{@TS.name}"
+          render 'writer_final.html.erb'
+        else
+          t.save
+        end
+      end
+      i+=4
+    end
+    i = 0
+    while i < epdkeys.size
+      name=params[epdkeys[i]]
+      type=params[epdkeys[i+1]]
+      if(EntryPoint.exists?(:name=>name, :entryType=>type))
+        t = EntryPoint.where(:name=>name, :entryType=>type)[0]
+        t.destroy
+      else
+        flash[:error]="Hotel does not exist"
+        @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @user = User.find(params[:id])
+        render 'writer_final.html.erb'
+      end
+      i+=2
+    end
+
+    # Now lets modify LocalTransportStands
+    i = 0
+    while i < ltskeys.size
+      name = params[ltskeys[i]]
+      lat = params[ltskeys[i+1]]
+      long = params[ltskeys[i+2]]
+      type = params[ltskeys[i+3]]
+      if(LocalTransportStand.exists?(:name=>name, :transportType=>type, :stateName=>session[:stateName], :districtName=>session[:districtName]))
+        t = LocalTransportStand.where(:name=>name, :transportType=>type, :stateName=>session[:stateName], :districtName=>session[:districtName])[0]
+        unless t.update_attributes(:latitude=>lat,:longitude=>long)
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of LocalTransportStand #{@TS.name}"
+          render 'writer_final.html.erb'
+        end
+      else
+        t = LocalTransportStand.new
+        t.name=name
+        t.latitude=lat
+        t.longitude=long
+        t.transportType=type
+        if t.save?
+          @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+          @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+          @user = User.find(params[:id])
+          flash[:error]="Invalid attributes of TouristSpot #{@TS.name}"
+          render 'writer_final.html.erb'
+        else
+          t.save
+        end
+      end
+      i+=4
+    end
+    i = 0
+    while i < ltsdkeys.size
+      name=params[tsdkeys[i]]
+      type=params[tsdkeys[i+1]]
+      if(LocalTransportStand.exists?(:name=>name, :transportType=>type, :stateName=>session[:stateName], :districtName=>session[:districtName]))
+        t = LocalTransportStand.where(:name=>name, :transportType=>type, :stateName=>session[:stateName], :districtName=>session[:districtName])[0]
+        t.destroy
+      else
+        flash[:error]="LocalTransportStand does not exist"
+        @TS= TouristSpot.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @EP=EntryPoint.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name)
+        @LTS = LocalTransportStand.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @H = Hotel.where(:districtName=>session[:districtName], :stateName=>session[:stateName]).pluck(:name);
+        @user = User.find(params[:id])
+        render 'writer_final.html.erb'
+      end
+      i+=2
+    end
+    
+    flash[:notice]="DONE!"
     redirect_to "/users/#{params[:id]}"
   end
 
